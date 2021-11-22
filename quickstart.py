5963821899b1c646ebb7238a7df635a78df956f3
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-11-21 18:47:23 -0600

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -2,5 +2,4 @@ from tinderbotz.session import Session
 
 if __name__ == "__main__":
     session = Session()                             # Instance of the Session Class
-
     session.like(amount=25, ratio="84.5%", sleep=3) # Edit amount (i.e., the number of times you want to swipe right), ratio (i.e, percentage chance of attempting a right swipe), and sleep (i.e., the number of seconds to wait between swipes)
